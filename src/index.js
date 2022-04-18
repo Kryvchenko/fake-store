967e077329b4ceba5dafbfee649bb91c73356eaa
Oleks Kryo <amzkryvchenko@gmail.com>
2022-04-18 10:58:38 -0300

    redux added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { App } from 'containers/App/App'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { productsLikeReducer } from 'redux/productsLikeReducer'
+
+const store = createStore(
+    productsLikeReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+)
 
 ReactDom.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 )
