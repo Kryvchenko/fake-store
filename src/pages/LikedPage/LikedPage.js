e316f5b767d5398f5583b1bf622366028b57ca7f
Oleks Kryo <amzkryvchenko@gmail.com>
2022-05-19 12:08:07 -0300

    bug fixed

--- src/pages/LikedPage/LikedPage.js
+++ src/pages/LikedPage/LikedPage.js
@@ -1,6 +1,6 @@
 import { Grid } from '@mui/material'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+
 import { CartProductList } from 'components/Cart/CartProductList'
 import { CartProductListItemExtended } from 'components/Cart/CartProductsListItemExtended'
 
